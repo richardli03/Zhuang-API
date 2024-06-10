bb32f5f88395d10b1353876913f9ca8160390831
richardli03 <richardli485@gmail.com>
2024-06-10 16:32:11 -0700

    implement CRUD

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def get_db():
 def read_root():
     return """Creating a budgetting application just to see if I can make a RESTful API"""
 
-@app.post("/categories/create/", response_model=CategoryModel)
+@app.post("/categories/", response_model=CategoryModel)
 def create_category(category: CategoryModel, db: Session = Depends(get_db)):
     db_category = Category(category_name=category.name)
     db.add(db_category)
@@ -39,10 +39,27 @@ def create_category(category: CategoryModel, db: Session = Depends(get_db)):
     db.refresh(db_category)
     return db_category
 
-@app.get("/categories/read/", response_model=List[CategoryModel])
+@app.get("/categories/", response_model=List[CategoryModel])
 def read_categories(limit: int = 10, db: Session = Depends(get_db)):
     categories = db.query(Category).limit(limit).all()
     return categories
+
+@app.put("/categories/{category_id}", response_model=CategoryModel)
+def update_categories(category_id: int, new_category_name: str,  db: Session = Depends(get_db)):
+    db_category = db.query(Category).filter_by(id=category_id).first()
+    if db_category:
+        db_category.name = new_category_name
+        db.commit()
+        db.refresh(db_category)
+    return db_category
+
+@app.delete("/categories/{category_id}")
+def delete_categories(category_id:int, db: Session = Depends(get_db)):
+    db_category = db.query(Category).filter_by(id=category_id).first()
+    if db_category:
+        db.delete(db_category)
+        db.commit()
+    return {"db_category": db_category, "status": "deleted"}
 
 @app.get("/categories/{category_id}/entries", response_model=List[EntryModel])
 def read_all_entries_in_category(category_id: int, db: Session = Depends(get_db)):
@@ -50,9 +67,7 @@ def read_all_entries_in_category(category_id: int, db: Session = Depends(get_db)
     if category:
         return category.entries
 
-    raise HTTPException(status_code=404, detail="Category not found")
-
-@app.post("/entries/create/", response_model=EntryModel)
+@app.post("/entries/", response_model=EntryModel)
 def create_entry(entry: EntryModel, db: Session = Depends(get_db)):
     db_entry = Entry(time = entry.time, recipient=entry.recipient, amount=entry.amount, description=entry.desc, category_id=entry.category_id)
     db.add(db_entry)
@@ -60,10 +75,32 @@ def create_entry(entry: EntryModel, db: Session = Depends(get_db)):
     db.refresh(db_entry)
     return db_entry
 
-@app.get("/entries/read/", response_model=List[EntryModel])
+@app.get("/entries/", response_model=List[EntryModel])
 def read_entries(limit: int = 10, db: Session = Depends(get_db)):
     entries = db.query(Entry).limit(limit).all()
     return entries
+
+@app.put("/entries/{entry_id}", response_model=EntryModel)
+def update_entry(entry_id: int, entry: EntryModel, db: Session = Depends(get_db)):
+    db_entry = db.query(Entry).filter_by(id=entry_id).first()
+    if db_entry:
+        db_entry.time = entry.time
+        db_entry.recipient = entry.recipient
+        db_entry.amount = entry.amount
+        db_entry.desc = entry.desc
+        db_entry.category_id = entry.category_id
+        db.commit()
+        db.refresh(db_entry)
+
+    return db_entry
+
+@app.delete("/entries/{entry_id}")
+def delete_entry(entry_id: int, db: Session = Depends(get_db)):
+    db_entry = db.query(Entry).filter_by(id=entry_id).first()
+    if db_entry:
+        db.delete(db_entry)
+        db.commit()
+        return db_entry
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
