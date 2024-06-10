c1a347f7c33ae629c05460644c6dafbaddca6a7a
richardli03 <richardli485@gmail.com>
2024-06-10 15:33:58 -0700

    add endpoint to get all entries with a specific category

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def get_db():
 def read_root():
     return """Creating a budgetting application just to see if I can make a RESTful API"""
 
-@app.post("/categories/", response_model=CategoryModel)
+@app.post("/categories/create/", response_model=CategoryModel)
 def create_category(category: CategoryModel, db: Session = Depends(get_db)):
     db_category = Category(category_name=category.name)
     db.add(db_category)
@@ -39,12 +39,20 @@ def create_category(category: CategoryModel, db: Session = Depends(get_db)):
     db.refresh(db_category)
     return db_category
 
-@app.get("/categories/", response_model=List[CategoryModel])
-def read_categories(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
-    categories = db.query(Category).offset(skip).limit(limit).all()
+@app.get("/categories/read/", response_model=List[CategoryModel])
+def read_categories(limit: int = 10, db: Session = Depends(get_db)):
+    categories = db.query(Category).limit(limit).all()
     return categories
 
-@app.post("/entries/", response_model=EntryModel)
+@app.get("/categories/{category_id}/entries", response_model=List[EntryModel])
+def read_all_entries_in_category(category_id: int, db: Session = Depends(get_db)):
+    category = db.query(Category).filter_by(id=category_id).first()
+    if category:
+        return category.entries
+
+    raise HTTPException(status_code=404, detail="Category not found")
+
+@app.post("/entries/create/", response_model=EntryModel)
 def create_entry(entry: EntryModel, db: Session = Depends(get_db)):
     db_entry = Entry(time = entry.time, recipient=entry.recipient, amount=entry.amount, description=entry.desc, category_id=entry.category_id)
     db.add(db_entry)
@@ -52,9 +60,9 @@ def create_entry(entry: EntryModel, db: Session = Depends(get_db)):
     db.refresh(db_entry)
     return db_entry
 
-@app.get("/entries/", response_model=List[EntryModel])
-def read_entries(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
-    entries = db.query(Entry).offset(skip).limit(limit).all()
+@app.get("/entries/read/", response_model=List[EntryModel])
+def read_entries(limit: int = 10, db: Session = Depends(get_db)):
+    entries = db.query(Entry).limit(limit).all()
     return entries
 
 if __name__ == "__main__":
