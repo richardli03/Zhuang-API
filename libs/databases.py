11b944407868353a29efc9e6b3d9d18771bd480a
richardli03 <rli@olin.edu>
2024-06-22 21:53:57 -0700

    redefine more robust databases

--- libs/databases.py
+++ libs/databases.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
 
+
 class Category(Base):
     __tablename__ = "categories"
 
@@ -16,6 +17,7 @@ class Category(Base):
     def __repr__(self) -> str:
         return f"Category: {self.name}, ID: {self.id}"
 
+
 class Exercise(Base):
     __tablename__ = "exercises"
 
@@ -25,26 +27,27 @@ class Exercise(Base):
     category = relationship("Category", back_populates="exercises")
     entries = relationship("Entry", back_populates="exercise")
 
-    def __init__(self, name, category) -> None:
+    def __init__(self, name, category_id) -> None:
         self.name = name
-        self.category = category
+        self.category_id = category_id
 
     def __repr__(self) -> str:
         return f"Exercise: {self.name}, ID: {self.id}"
+
 
 class Entry(Base):
     __tablename__ = "entries"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     time = Column(DateTime, index=True)
-    name = Column(String, ForeignKey("exercises.name"))
+    exercise_id = Column(String, ForeignKey("exercises.id"))
     set_info = Column(JSON)
     exercise = relationship("Exercise", back_populates="entries")
 
-    def __init__(self, time, name, set_info) -> None:
+    def __init__(self, time, name, exercise_id, set_info) -> None:
         self.time = time
-        self.name = name
+        self.exercise_id = exercise_id
         self.set_info = set_info
 
     def __repr__(self) -> str:
-        return f"Entry ID: {self.id}, Exercise: {self.name}, Time: {self.time}"
+        return f"Entry ID: {self.id}, Exercise {self.exercise_id}: {self.exercise.name}, Time: {self.time}"
--- scripts/database_runner.py
+++ scripts/database_runner.py
@@ -1,3 +1,5 @@
+""" just adding a couple of database entries to work with the api easier"""
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
 from datetime import datetime
@@ -15,10 +17,18 @@ def main():
     session = create_session("richard_workouts", True)
 
     # Create a category
-    chest_category = Category(name="Chest")
+    chest = Category(name="Chest")
+    legs = Category(name="Legs")
+    arms = Category(name="Arms")
+    session.add(chest)
+    session.add(legs)
+    session.add(arms)
+    # committing to the session also adds the id
+    session.commit()
 
-    # Create an exercise
-    bench_press = Exercise(name="Bench Press", category=chest_category)
+    bench_press = Exercise(name="Bench Press", category_id=chest.id)
+    dips = Exercise(name="Dips", category_id=chest.id)
+    incline_press = Exercise(name="Incline Press", category_id=chest.id)
 
     # Create an entry with set info
     entry = Entry(
@@ -30,12 +40,39 @@ def main():
             {"weight": 150, "reps": 5},
             {"weight": 150, "reps": 5},
         ],
+        exercise_id=1,
+    )
+    entry1 = Entry(
+        time=datetime.now(),
+        name=bench_press.name,
+        set_info=[
+            {"weight": 140, "reps": 5},
+            {"weight": 140, "reps": 7},
+            {"weight": 140, "reps": 7},
+            {"weight": 140, "reps": 7},
+        ],
+        exercise_id=1,
+    )
+    entry2 = Entry(
+        time=datetime.now(),
+        name=bench_press.name,
+        set_info=[
+            {"weight": 150, "reps": 8},
+            {"weight": 150, "reps": 8},
+            {"weight": 150, "reps": 5},
+            {"weight": 150, "reps": 5},
+        ],
+        exercise_id=2,
     )
 
     # Add to session and commit
-    session.add(chest_category)
+
     session.add(bench_press)
+    session.add(dips)
+    session.add(incline_press)
     session.add(entry)
+    session.add(entry1)
+    session.add(entry2)
     session.commit()
 
 
