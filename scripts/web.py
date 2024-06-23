c9a6b09e5cca01d6f9365fde37da6eda914429c1
richardli03 <rli@olin.edu>
2024-06-22 22:24:21 -0700

    start API interface in python

---
+++ scripts/web.py
@@ -0,0 +1,71 @@
+import requests
+import json
+from pprint import pprint
+from datetime import datetime
+from libs.schemas import EntryInput
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+
+URL = "http://127.0.0.1:8000"
+
+
+def get_categories():
+    response = requests.get(f"{URL}/categories/")
+    return response.json()
+
+
+def get_exercises():
+    response = requests.get(f"{URL}/exercises/")
+    return response.json()
+
+
+def get_entries():
+    response = requests.get(f"{URL}/entries/")
+    return response.json()
+
+
+def create_category(category_name: str):
+    """Remember, `params` are useful for when the input to your
+    request is simple enough to be included in the URL
+
+    Args:
+        category_name (str): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    response = requests.post(
+        f"{URL}/categories/",
+        params={"category_name": category_name},
+        headers={"accept": "application/json"},
+    )
+    return response
+
+
+def create_entry(entry_name, set_info: list[dict]):
+    entry = {"name": entry_name, "set_info": set_info}
+    response = requests.post(
+        f"{URL}/entries/",
+        headers={"Content-Type": "application/json", "accept": "application/json"},
+        json=entry,
+    )
+    return response.json()
+
+
+set_info = [
+    {"weight": 30, "reps": 12},
+    {"weight": 30, "reps": 12},
+    {"weight": 30, "reps": 12},
+]
+
+entry_name = "Bench Press"
+
+create_entry(entry_name, set_info)
+
+
+# create_a_category("Test3")
+
+
+pprint(get_entries())
