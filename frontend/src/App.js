128ad702d48715cec8a990bd26008e234424da8c
richardli03 <richardli485@gmail.com>
2024-07-06 14:18:33 -0700

    add simple frontend app + ping

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,28 @@
 import logo from './logo.svg';
+import axios from 'axios';
+import React, { useState} from 'react';
 import './App.css';
 
 function App() {
+  const [alive, setStatus] = useState(null);
+
+  const pingServer = async () => {
+    const response = await axios.get('http://127.0.0.1:8000');
+    setStatus(response.data);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button className="ping-check-button" onClick={pingServer}>
+        ping server
+        </button>
+        {alive && (
+          <div className={`ping-status ${alive === 'ping' ? 'ping' : 'error'}`}>
+            response from api: {alive}
+          </div>
+        )}
       </header>
     </div>
   );
